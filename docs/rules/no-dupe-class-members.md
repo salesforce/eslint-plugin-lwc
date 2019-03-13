89f5df83ee68d7ca4cac9a6a4304c2ab96039065
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2019-03-13 07:56:52 +0100

    Add no-dupe-class-member rule (#11)
    * add no-dupe-class-member rule
    
    * address review comments

---
+++ docs/rules/no-dupe-class-members.md
@@ -0,0 +1,45 @@
+# Disallow duplicate class members (no-dupe-class-members)
+
+If there are declarations of the same name in class members, the last declaration overwrites other declarations silently. This can cause unexpected behaviors. This rule prevents usage of duplicate class members (fields and methods) on the same class.
+
+> Note: This rule extends the original eslint rule [`no-dupe-class-members`](https://eslint.org/docs/rules/no-dupe-class-members) to add support for [class fields](https://github.com/tc39/proposal-class-fields) that are not yet stage 4.
+
+## Rule details
+
+Example of **incorrect** code:
+
+```js
+class Foo {
+    bar() {}
+    bar() {}
+}
+
+class Foo {
+    bar;
+    bar() {}
+}
+
+class Foo {
+    bar;
+    get bar() {}
+}
+```
+
+Example of **correct** code:
+
+```js
+class Foo {
+    foo() {}
+    bar() {}
+}
+
+class Foo {
+    foo;
+    bar() {}
+}
+
+class Foo {
+    foo;
+    get bar() {}
+}
+```
---
+++ test/lib/rules/no-dupe-class-members.js
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+'use strict';
+
+const { RuleTester } = require('eslint');
+
+const { ESLINT_TEST_CONFIG } = require('../shared');
+const rule = require('../../../lib/rules/no-dupe-class-members');
+
+const ruleTester = new RuleTester(ESLINT_TEST_CONFIG);
+
+ruleTester.run('no-dupe-class-members', rule, {
+    valid: [
+        // Original rule tests:
+        // https://github.com/eslint/eslint/blob/2c7431d6b32063f74e3837ee727f26af215eada7/tests/lib/rules/no-dupe-class-members.js#L23
+        'class A { foo() {} bar() {} }',
+        'class A { static foo() {} foo() {} }',
+        'class A { get foo() {} set foo(value) {} }',
+        'class A { static foo() {} get foo() {} set foo(value) {} }',
+        'class A { foo() { } } class B { foo() { } }',
+        'class A { [foo]() {} foo() {} }',
+        "class A { 'foo'() {} 'bar'() {} baz() {} }",
+        "class A { *'foo'() {} *'bar'() {} *baz() {} }",
+        "class A { get 'foo'() {} get 'bar'() {} get baz() {} }",
+        'class A { 1() {} 2() {} }',
+
+        // Custom logic with class fields
+        'class A { foo; bar; }',
+        'class A { foo; [foo]; }',
+        'class A { foo; bar() {} }',
+        'class A { foo; static foo; }',
+        'class A { foo; static foo() {} }',
+        "class A { 'foo'; static 'foo'; }",
+        "class A { 'foo'; static 'foo'() {} }",
+        'class A { static foo; static bar; }',
+        "class A { static 'foo'; static 'bar'; }",
+    ],
+    invalid: [
+        // Original rule tests:
+        // https://github.com/eslint/eslint/blob/2c7431d6b32063f74e3837ee727f26af215eada7/tests/lib/rules/no-dupe-class-members.js#L35
+        {
+            code: 'class A { foo() {} foo() {} }',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 20,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: '!class A { foo() {} foo() {} };',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 21,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: "class A { 'foo'() {} 'foo'() {} }",
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 22,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: 'class A { 10() {} 1e1() {} }',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 19,
+                    messageId: 'unexpected',
+                    data: { name: '10' },
+                },
+            ],
+        },
+        {
+            code: 'class A { foo() {} foo() {} foo() {} }',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 20,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 29,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: 'class A { static foo() {} static foo() {} }',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 27,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: 'class A { foo() {} get foo() {} }',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 20,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: 'class A { set foo(value) {} foo() {} }',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 29,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+
+        // Custom logic with class fields
+        {
+            code: 'class A { foo; foo; }',
+            errors: [
+                {
+                    type: 'ClassProperty',
+                    line: 1,
+                    column: 16,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: 'class A { foo; foo() {} }',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 16,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: 'class A { foo; get foo() {} set foo(v) {} }',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 16,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 29,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: "class A { 'foo'; foo() {} }",
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 18,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+        {
+            code: 'class A { static foo; static foo() {} }',
+            errors: [
+                {
+                    type: 'MethodDefinition',
+                    line: 1,
+                    column: 23,
+                    messageId: 'unexpected',
+                    data: { name: 'foo' },
+                },
+            ],
+        },
+    ],
+});
