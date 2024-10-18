d05b230017e0ad1210daeb06f4b59ae02f31f5f4
Lesya Turanscaia <turanscaia.lesya@gmail.com>
2024-10-18 13:58:18 -0700

    New rule 'no unsupported Node APIs in SSR-able components' (#167)
    * New rule 'no-unguarded risky Node APIs in SSR-able components'
    
    * Block the risky Node API in SSR-able components
    
    * Change 'risky' to 'unsupported'
    
    * Update docs/rules/no-unsupported-node-api-in-ssrable-components.md
    
    Co-authored-by: Laura <49494194+lpomerleau@users.noreply.github.com>
    
    * Addressing the feedback
    
    * rename the rule
    
    ---------
    
    Co-authored-by: lturanscaia <lturanscaia@salesforce.com>
    Co-authored-by: Laura <49494194+lpomerleau@users.noreply.github.com>

---
+++ test/lib/rules/no-unsupported-node-api.js
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+'use strict';
+
+const { testRule, testTypeScript } = require('../shared');
+
+testRule('ssr-no-unsupported-node-api', {
+    valid: [
+        {
+            code: `
+                // Safe code without the unsupported Node API calls
+                if (import.meta.env.SSR) {
+                    // Safe code
+                }
+            `,
+        },
+    ],
+    invalid: [
+        {
+            code: `
+                const fs = require('fs');
+
+                // the unsupported Node API call
+                fs.writeFileSync('file.txt', 'data');
+            `,
+            errors: [
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                    line: 2,
+                },
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                    line: 5,
+                },
+            ],
+        },
+        {
+            code: `
+               require('something');
+            `,
+            errors: [
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                },
+            ],
+        },
+        {
+            code: `
+                const fs = require('node:fs');
+
+                // Unsupported Node API call with node: prefix
+                fs.writeFileSync('file.txt', 'data');
+            `,
+            errors: [
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                    line: 2,
+                },
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                    line: 5,
+                },
+            ],
+        },
+        {
+            code: `
+                require('node:child_process');
+            `,
+            errors: [
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                },
+            ],
+        },
+    ],
+});
+
+testTypeScript('ssr-no-unsupported-node-api', {
+    valid: [
+        {
+            code: `
+                // Safe code without the unsupported Node API calls
+                if (import.meta.env.SSR) {
+                    // Safe code
+                }
+            `,
+        },
+    ],
+    invalid: [
+        {
+            code: `
+                const fs = require('fs');
+
+                // the unsupported Node API call
+                fs.writeFileSync('file.txt', 'data');
+            `,
+            errors: [
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                    line: 2,
+                },
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                    line: 5,
+                },
+            ],
+        },
+        {
+            code: `
+                const fs = require('node:fs');
+
+                // the unsupported Node API call with node: prefix in TypeScript
+                fs.writeFileSync('file.txt', 'data');
+            `,
+            errors: [
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                    line: 2,
+                },
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                    line: 5,
+                },
+            ],
+        },
+        {
+            code: `
+                require('node:child_process');
+            `,
+            errors: [
+                {
+                    message:
+                        'The unsupported Node API calls are not allowed in SSR-able components.',
+                },
+            ],
+        },
+    ],
+});
