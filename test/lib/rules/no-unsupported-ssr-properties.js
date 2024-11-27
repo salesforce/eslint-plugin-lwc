8dec5dc93103f49d561b1e35fa19c80765144326
achab9 <a.chabot@salesforce.com>
2024-11-27 10:45:32 -0500

    expand conditional statements

--- test/lib/rules/no-unsupported-ssr-properties.js
+++ test/lib/rules/no-unsupported-ssr-properties.js
@@ -183,6 +183,19 @@ testRule('no-unsupported-ssr-properties', {
               }
           `,
         },
+        {
+            code: `
+              import { LightningElement } from 'lwc';
+              
+              export default class Foo extends LightningElement {
+                connectedCallback() {
+                  if (randomOtherCheck && typeof window !== 'undefined') {
+                    this.querySelector('span').getAttribute('role');
+                  }
+                }
+              }
+            `,
+        },
     ],
     invalid: [
         {
@@ -505,6 +518,24 @@ testRule('no-unsupported-ssr-properties', {
                   }
                 }
               }
+          `,
+          errors: [
+            {
+                messageId: 'propertyAccessFound',
+            }
+          ]
+        },
+        {
+          code: `
+            import { LightningElement } from 'lwc';
+            
+            export default class Foo extends LightningElement {
+              connectedCallback() {
+                if (randomOtherCheck && typeof window == 'undefined') {
+                  this.querySelector('span').getAttribute('role');
+                }
+              }
+            }
           `,
           errors: [
             {
