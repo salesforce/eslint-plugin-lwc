409927bf5dffd06a0df649ee52c3a34c5c3a72af
Ayush Bhagta <58397163+abhagta-sfdc@users.noreply.github.com>
2025-01-23 23:59:02 +0530

    @W-17471447 Add a ssr processor and update ssr rule (#185)

---
+++ docs/processors/ssr.md
@@ -0,0 +1,130 @@
+# `ssr` Processor
+
+## Overview
+
+The `ssr` processor is designed to streamline the linting of SSR-capable Lightning Web Components (LWCs). By reading the metadata in the `js-meta.xml` file, the processor identifies whether a component supports Server-Side Rendering (SSR). For SSR-capable components, the processor generates virtual files in memory with a .ssrjs extension. These virtual files allow you to apply SSR-specific linting rules in a targeted manner.
+
+## Key Features
+
+-   SSR Capability Detection: The processor identifies components with SSR capabilities based on the js-meta.xml file.
+
+-   Virtual File Creation: For each SSR-capable component, the processor creates corresponding .ssrjs virtual files.
+
+-   Targeted Linting: These .ssrjs files can be processed with SSR-specific ESLint rules.
+
+### **Usage**
+
+The processor helps to specifically target ssr js files to apply ssr specific rules by reading `js-meta.xml` files of components.
+
+**Example:**
+
+```xml
+<LightningComponentBundle xmlns="http://soap.sforce.com/2006/04/metadata">
+    <apiVersion>50.0</apiVersion>
+    <isExposed>true</isExposed>
+    <targets>
+        <target>lightning__RecordPage</target>
+        <target>lightning__AppPage</target>
+        <target>lightning__RecordPage</target>
+    </targets>
+    <capabilities>
+        <capability>lightning__ServerRenderableWithHydration</capability> <!-- Indicate SSR capability here -->
+    </capabilities>
+</LightningComponentBundle>
+```
+
+In this example, the capabilities tag indicates whether the component supports SSR. If either of the following capabilities is defined:
+
+-   lightning\_\_ServerRenderable
+-   lightning\_\_ServerRenderableWithHydration
+
+The processor will generate a `${filename}.ssrjs` virtual file for each js file associated with the component.
+
+### Configuration Example
+
+To configure the processor, ensure your ESLint configuration includes @lwc/eslint-plugin-lwc/ssr as a processor:
+
+Flat config:
+
+```
+import lwcPlugin from '@lwc/eslint-plugin-lwc';
+export default [
+  {
+    files: ['**/modules/**/*.js'],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        requireConfigFile: false,
+      },
+    },
+    plugins: {
+      lwcPlugin
+    },
+    processor: lwcPlugin.processors.ssr
+  },
+  {
+    files: ['**/modules/**/*.ssrjs'],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        requireConfigFile: false,
+      },
+    },
+    plugins: {
+      lwcPlugin
+    },
+    rules: {
+          "no-console" : "error",
+          "lwcPlugin/ssr-no-node-env": "error"
+        }
+  }
+];
+```
+
+Non-flat config:
+
+```
+module.exports = {
+  overrides: [
+    {
+      files: ['**/modules/**/*.js'],
+      parser: '@babel/eslint-parser',
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        requireConfigFile: false,
+      },
+      plugins: ['@lwc/lwc'],
+      processor: '@lwc/lwc/ssr'
+    },
+    {
+      files: ['**/modules/**/*.ssrjs'],
+      parser: '@babel/eslint-parser',
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        requireConfigFile: false,
+      },
+      plugins: ['@lwc/lwc'],
+      rules: {
+        "no-console": "error",
+        "@lwc/lwc/ssr-no-node-env": "error"
+      }
+    }
+  ]
+};
+```
+
+### Explanation of Configuration
+
+-   Processor Configuration:
+
+The processor key applies the ssr processor to all `**/modules/**/*.js` files. The processor then created a virtual files for all js files of ssrable components with `.ssrjs` extension/
+
+-   Targeted Linting for .ssrjs Files:
+
+A separate configuration block applies specific rules to these virtual `.ssrjs` files, ensuring SSR-specific rules like `lwc/ssr-no-node-env` are only applied SSR capable components.
---
+++ test/lib/processors/ssr.test.js
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
+'use strict';
+const { expect } = require('chai');
+const fs = require('fs');
+const sinon = require('sinon');
+const ssrProcessor = require('../../../lib/processors/ssr');
+
+describe('JS Meta XML Processor with Capabilities Check', () => {
+    let fsReadFileSync;
+    let fsReadDirSync;
+
+    beforeEach(() => {
+        fsReadFileSync = sinon.stub(fs, 'readFileSync');
+        fsReadDirSync = sinon.stub(fs, 'readdirSync');
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    describe('preprocess', () => {
+        it('should process and create a new file with ssrjs extension if file is ssrable', () => {
+            const input = 'const x = 1;';
+            const filename = 'test/test.js';
+            const validMetaXML = `
+                <?xml version="1.0" encoding="UTF-8"?>
+                <LightningComponentBundle xmlns="http://soap.sforce.com/2006/04/metadata">
+                <capabilities>
+                    <capability>lightning__ServerRenderable</capability>
+                </capabilities>
+                </LightningComponentBundle>
+            `;
+
+            fsReadFileSync.returns(validMetaXML);
+            fsReadDirSync.returns(['test.js-meta.xml']);
+
+            const result = ssrProcessor.preprocess(input, filename);
+
+            expect(result).to.have.lengthOf(2);
+            expect(result[1]).to.deep.equal({
+                filename: 'test.ssrjs',
+                text: 'const x = 1;',
+            });
+            expect(result[0]).to.deep.equal(input);
+        });
+
+        it('should skip creating new virtual file with ssrjs extension when meta exists but does not have ssr capability', () => {
+            const input = 'const x = 1;';
+            const filename = 'test1/test.js';
+            const invalidMetaXML = `
+                <?xml version="1.0" encoding="UTF-8"?>
+                <LightningComponentBundle xmlns="http://soap.sforce.com/2006/04/metadata">
+                </LightningComponentBundle>
+            `;
+
+            fsReadFileSync.returns(invalidMetaXML);
+            fsReadDirSync.returns(['test.js-meta.xml']);
+
+            const result = ssrProcessor.preprocess(input, filename);
+
+            expect(result).to.have.lengthOf(1);
+            expect(result[0]).to.deep.equal(input);
+        });
+
+        it('should handle array of capabilities', () => {
+            const input = 'const x = 1;';
+            const filename = 'test2/test.js';
+            const multipleCapabilitiesXML = `
+                <?xml version="1.0" encoding="UTF-8"?>
+                <LightningComponentBundle xmlns="http://soap.sforce.com/2006/04/metadata">
+                    <capabilities>
+                        <capability>lightning__dynamicComponent</capability>
+                        <capability>lightning__ServerRenderableWithHydration</capability>
+                    </capabilities>
+                </LightningComponentBundle>
+            `;
+
+            fsReadFileSync.returns(multipleCapabilitiesXML);
+            fsReadDirSync.returns(['test.js-meta.xml']);
+
+            const result = ssrProcessor.preprocess(input, filename);
+
+            expect(result).to.have.lengthOf(2);
+        });
+
+        it('should use cached result for file in same directory', () => {
+            const input = 'const x = 1;';
+            const filename = 'tests/test.js';
+            const validMetaXML = `
+                <?xml version="1.0" encoding="UTF-8"?>
+                <LightningComponentBundle xmlns="http://soap.sforce.com/2006/04/metadata">
+                <capabilities>
+                    <capability>lightning__ServerRenderable</capability>
+                </capabilities>
+                </LightningComponentBundle>
+            `;
+
+            fsReadFileSync.returns(validMetaXML);
+            fsReadDirSync.returns(['test.js-meta.xml']);
+
+            const result = ssrProcessor.preprocess(input, filename);
+
+            expect(result).to.have.lengthOf(2);
+            expect(fsReadFileSync.callCount).to.equal(1);
+            expect(fsReadDirSync.callCount).to.equal(1);
+
+            // Second call with different file in same directory
+            const input2 = 'const y = 2;';
+            const filename2 = 'tests/test2.js';
+
+            const result2 = ssrProcessor.preprocess(input2, filename2);
+
+            // Verify second call
+            expect(result2).to.have.lengthOf(2);
+            // Verify fs functions weren't called again, i.e ssr check data for `tests` dir is picked from cached data from last run
+            expect(fsReadFileSync.callCount).to.equal(1, 'readFileSync should not be called again');
+            expect(fsReadDirSync.callCount).to.equal(1, 'readdirSync should not be called again');
+
+            // Verify results structure
+            expect(result2[0]).to.deep.equal(input2);
+            expect(result2[1]).to.deep.equal({
+                text: input2,
+                filename: 'test2.ssrjs',
+            });
+        });
+        it('should return same file for virtual file .ssrjs extension', () => {
+            const input = 'const x = 1;';
+            const filename = 'test2/test.ssrjs';
+            const testXML = `
+                <?xml version="1.0" encoding="UTF-8"?>
+                <LightningComponentBundle xmlns="http://soap.sforce.com/2006/04/metadata">
+                    <capabilities>
+                        <capability>lightning__ServerRenderableWithHydration</capability>
+                    </capabilities>
+                </LightningComponentBundle>
+            `;
+
+            fsReadFileSync.returns(testXML);
+            fsReadDirSync.returns(['test.js-meta.xml']);
+
+            const result = ssrProcessor.preprocess(input, filename);
+            expect(result[0]).to.deep.equal(input);
+        });
+    });
+});
